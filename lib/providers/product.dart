f9693b1625f6590993427079ec23627f658ae4ae
yankzsoe <yayang.suryana@gic-indonesia.com>
2021-04-25 21:10:32 +0700

    updating "isFavorite" product to backend.

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +19,28 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
     notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldFavorite = isFavorite;
+    _setFavValue(!isFavorite);
+    final url = Uri.parse(
+        'https://flutter-auth-ce5c5-default-rtdb.firebaseio.com/products/$id.json');
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldFavorite);
+      }
+    } catch (error) {
+      _setFavValue(oldFavorite);
+    }
   }
 }
