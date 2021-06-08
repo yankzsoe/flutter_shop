fc2a12ce8b2c00c2f0ea0683664f22eb48569486
yankzsoe <yayang.suryana@gic-indonesia.com>
2021-06-08 07:57:54 +0700

    add animation transition on page transition using PageTransitionsTheme.

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './screens/edit_product_screen.dart';
 import './screens/auth_screen.dart';
 import './providers/auth.dart';
 import './screens/splash_screen.dart';
+import './tools/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -46,6 +47,10 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.purple,
               accentColor: Colors.deepOrange,
               fontFamily: 'Lato',
+              pageTransitionsTheme: PageTransitionsTheme(builders: {
+                TargetPlatform.android: CustomPageTransitionBuilder(),
+                TargetPlatform.iOS: CustomPageTransitionBuilder(),
+              }),
             ),
             home: auth.isAuth
                 ? ProductsOverviewScreen()
